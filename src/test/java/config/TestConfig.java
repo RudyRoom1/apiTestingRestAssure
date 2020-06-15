8363c8041cd21783dd71180b515b504657a4825c
Yaroslav Kush <yaroslav_kush@epam.com>
2020-06-15 16:15:01 +0300

    Init

---
+++ src/test/java/config/TestConfig.java
@@ -0,0 +1,25 @@
+package config;
+
+import io.cucumber.java.Before;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+public class TestConfig {
+    public static RequestSpecification videoGame_requestSpec;
+
+    @Before
+    public static void beforeScenario() {
+        System.out.println("before");
+
+        videoGame_requestSpec = new RequestSpecBuilder().
+                setBaseUri("http://localhost").
+                setPort(8080).
+                setBasePath("/app/").
+                addHeader("Content-Type", "application/json").
+                addHeader("Accept", "application/json").
+                build();
+
+    }
+
+
+}
---
+++ src/test/java/randomuser_models/RandonUser.java
@@ -0,0 +1,37 @@
+package randomuser_models;
+
+import java.util.HashMap;
+    import java.util.List;
+    import java.util.Map;
+
+public class RandonUser {
+
+  private List<Result> results = null;
+  private Info info;
+  private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+  public List<Result> getResults() {
+    return results;
+  }
+
+  public void setResults(List<Result> results) {
+    this.results = results;
+  }
+
+  public Info getInfo() {
+    return info;
+  }
+
+  public void setInfo(Info info) {
+    this.info = info;
+  }
+
+  public Map<String, Object> getAdditionalProperties() {
+    return this.additionalProperties;
+  }
+
+  public void setAdditionalProperty(String name, Object value) {
+    this.additionalProperties.put(name, value);
+  }
+
+}
